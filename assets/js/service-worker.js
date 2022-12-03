06d67b42d955ef3b859ad53a673fefa263f28bcb
bob <robertcscott@gmail.com>
2022-12-03 12:05:50 -0500

    update service worker cache

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -15,7 +15,7 @@
 // Change to v2, etc. when e will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v3';
-const RUNTIME = 'runtime-v3';
+const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
