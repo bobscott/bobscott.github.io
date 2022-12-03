91c00c3e261719f43e27d28593df89fc8581843a
bob <robertcscott@gmail.com>
2022-12-03 12:10:36 -0500

    update service worker cache

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -20,9 +20,8 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  './', // Alias for index.html
-  'style.css',
-  'script.js'
+  'assets/css/style.css',
+  'assets/js/script.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
